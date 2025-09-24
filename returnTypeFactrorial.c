0cc8e153546b17a1f647964fd81700d0359a20b3
Subhajit Das <subhajit9004@gmail.com>
2025-09-24 23:47:41 +0530

    Add files via upload

---
+++ returnTypeFactrorial.c
@@ -0,0 +1,24 @@
+// return type :
+// factorial find:
+#include <stdio.h>
+long long factorial(int n)
+{
+    long long fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact; 
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    long long result = factorial(num); 
+    printf("Factorial of %d = %lld\n", num, result);
+
+    return 0;
+}
